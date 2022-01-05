240e57cc45cb57903984af71033a4c6e52b4b397
Lucas Vieira <l_vieira1998@hotmail.com>
2022-01-05 21:52:21 +0800

    careers styling

--- src/views/pages/careers.js
+++ src/views/pages/careers.js
@@ -15,8 +15,77 @@ class CareersView {
     const template = html`
       <va-app-header title="Careers" user="${JSON.stringify(Auth.currentUser)}"></va-app-header>
       <div class="page-content">        
-        <h1>Career Paths</h1>
-        <p>Page content ...</p>
+        <div class="careers-header">
+          <div class="header-info">
+            <h1>CAREERS</h1>
+            <p>We offer exciting and rewarding careers for talented individuals who wish to help improve Australia’s future through renewable energy.</p>
+            <span class="down-arrow">&#8595;</span>
+          </div>
+        </div>
+
+        <div class="careers-information">
+          <h2>Something something something...</h2>
+          <div class="careers-boxes-container">
+
+            <div class="careers-box">
+              <h3>Meaningful Work</h3>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum 
+                deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+            </div>
+
+            <div class="careers-box">
+              <h3>Meaningful Work</h3>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum 
+                deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+            </div>
+
+            <div class="careers-box">
+              <h3>Meaningful Work</h3>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum 
+                deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+            </div>
+            
+            <div class="careers-box">
+              <h3>Meaningful Work</h3>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum 
+                deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="careers-testimonials">
+          <h2>What our employees have to say</h2>
+          <hr>
+          <div class="testimonials-container">
+
+            <div class="testimonials-boxes border-right ">
+              <div class="testimonials-img"></div>
+              <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum 
+                deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident".</p>
+                <h4>David Green</h4>
+                <h5>Accounts Manager</h5>
+            </div>
+
+            <div class="testimonials-boxes border-right">
+            <div class="testimonials-img"></div>
+              <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum 
+                deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident".</p>
+                <h4>David Green</h4>
+                <h5>Accounts Manager</h5>
+            </div>
+
+            <div class="testimonials-boxes">
+            <div class="testimonials-img"></div>
+              <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum 
+                deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident".</p>
+                <h4>David Green</h4>
+                <h5>Accounts Manager</h5>
+            </div>
+
+          </div>
+
+        </div>
         
       </div>      
     `
