91f42fd110f32d63bb89dac09637fefdc0e60dc0
Ryan <s3766659@student.rmit.edu.au>
2022-02-10 17:28:01 +0800

    Added content to fossil fuels, hydrogen, solarpower and windpower still needs styling

--- src/views/pages/solarpower.js
+++ src/views/pages/solarpower.js
@@ -94,12 +94,60 @@ class SolarpowerView {
           <div class='solarpower-background'>
 
             <h1 class='anim-in'>Solar Power</h1>
+            <img id='solar-power-header-image' src='/images/solar-power-header.png' alt='Image of cartoon solar panels and sun.'>
 
           </div>
           
           <!-- scrolling content text, images and links          -->
           <div id='solarpower-body'>
-            <p>body text here</p>
+              <p>The amount of sunlight that strikes the earth's surface in an hour and a half is enough to handle 
+              the entire world's  energy consumption for a full year. Solar technologies convert sunlight into 
+              electrical energy either through photovoltaic (PV) panels or through mirrors that concentrate 
+              solar radiation. This energy can be used to generate electricity or be stored in batteries or thermal
+              storage. </p>
+              <p>Solar radiation is light – also known as electromagnetic radiation – that is emitted by the sun.
+              While every location on Earth receives some sunlight over a year, the amount of solar radiation 
+              that reaches any one spot on the Earth’s surface varies. Solar technologies capture this radiation
+              and turn it into useful forms of energy.</p>
+              <p>At the end of 2020, there was more than 700 GW of solar installed around the world, meeting 
+              around 3 percent of global electricity demand.
+              </p>
+              <p>More solar PV energy is added each year than any other type of energy generation, thanks largely
+              to the rapid cost reductions that have been achieved in recent years. Between 2010 and 2019, the 
+              global levelized cost of electricity (LCOE) for large scale solar dropped by 85 per cent.</p>
+              <p>There are two main types of solar power technology, solar photovoltaic and solar thermal.
+              </p>
+              <p>1. Solar photovoltaic
+                Solar photovoltaic (also known as solar PV) converts sunlight directly into electricity using a 
+                technology known as a semiconductor cell or solar PV cell. The most common form of solar PV
+                cell is typically encased in glass and an aluminium frame to form a solar panel. One or more panels 
+                can be installed to power a single light, cover the roof of a house for residential use, or be assembled 
+                into a large-scale solar farm generating hundreds of megawatts of  electricity. Solar PV panels are 
+                currently the most widespread type of solar PV technology, however other types of solar PV are
+                being developed for targeted applications including PV that can be integrated into buildings, flexible
+                PV and even PV paint.</p>
+
+                <div class='calign'>
+                  <img class='diagram' id='photovoltaic-image' src='/images/photovoltaic.png' alt='Image of photovoltaic solar panels.'>
+                </div>
+                <h4>PHOTOVOLTAIC</h4>
+                <p>(Open in a new window: https://arena.gov.au/renewable-energy/solar-pv-rd/)</p>
+
+                <p>2- Solar Thermal
+                  Solar thermal converts sunlight into heat (also known as thermal energy), which can be used for 
+                  a variety of purposes including creating steam to drive an electricity generator. This energy can 
+                  be used to drive a refrigeration cycle to provide solar-based cooling. There are two main types of 
+                  solar thermal technologies. Small scale thermal technology is used to heat space or water (such as
+                  in a solar hot water system). Concentrated solar thermal harvests the sun’s heat to  produce large-
+                  scale power generation. It uses a field of mirrors to reflect sunlight onto a device called a receiver, 
+                  which transfers the heat to a thermal energy storage system. Energy can then be released from 
+                  storage as required.</p>
+
+                  <div class='calign'>
+                  <img class='diagram' id='solar-thermal-image' src='/images/SOLAR THERMAL_IMAGE.png' alt='Image of solar thermal solar panels.'>
+                </div>
+                <h4>SOLAR THERMAL</h4>
+                <p>(Open in a new window: https://arena.gov.au/renewable-energy/concentrated-solar-thermal/)</p>
           </div>
         </div>  
       </div>
--- src/views/pages/windpower.js
+++ src/views/pages/windpower.js
@@ -94,14 +94,109 @@ class WindpowerView {
           <div class='windpower-background'>
 
             <h1 class='anim-in'>Wind Power</h1>
+            <img  id='wind-power-header-image' src='/images/wind-power-header.png' alt='Image of cartoon wind turbines.'>
 
           </div>
           
           <!-- scrolling content text, images and links          -->
           <div id='windpower-body'>
 
-            <p>body text here</p>
-
+            <h4>How do wind turbines work?</h4>
+            <br>
+            <p>Wind turbines work on a simple principle: instead of using electricity to make wind-like a fan-wind turbines use wind to make 
+            electricity. Wind turns the propeller-like blades of a turbine around a rotor, which spins a generator, which creates electricity.</p>
+            <br>
+            <p>Wind is a form of solar energy caused by a combination of three concurrent events:
+              <br>1- The sun unevenly heating the atmosphere.
+              <br>2- Irregularities of the earth's surface.
+              <br>3- The rotation of the earth.</p>
+              <br>
+            <p>The terms "wind energy" and "wind power" both describe the process by which the wind is used to generate mechanical power 
+            or electricity. This mechanical power can be used for specific tasks (such as grinding grain or pumping water) or a generator can 
+            convert this mechanical power into electricity.</p>
+            <br>
+            <p>A wind turbine turns wind energy into electricity using the aerodynamic force from the rotor blades, which work like an airplane 
+            wing or helicopter rotor blade. When wind flows across the blade, the air pressure on one side of the blade decreases. The
+            difference in air pressure across the two sides of the blade creates both lift and drag. The force of the lift is stronger than the drag
+            and this causes the rotor to spin. The rotor connects to the generator, either directly (if it’s a direct drive turbine) or through a shaft 
+            and a series of gears (a gearbox)  that speed up the rotation and allow for a physically smaller generator. This translation of 
+            aerodynamic force to rotation of a generator creates electricity. 
+            </p><br><br>
+            <h4>Advantages of Wind Power:</h4>
+            <br>
+            <p>Wind power is cost-effective. Land-based utility-scale wind is one of the lowest-priced energy sources available today, costing 
+            1–2 cents per kilowatt-hour after the production tax credit. Because the electricity from wind farms is sold at a fixed price over a 
+            long period of time (e.g. 20+ years) and its fuel is free, wind energy mitigates the price uncertainty that fuel costs add to traditional 
+            sources of energy.</p>
+            <br>
+            <p>
+              Wind creates jobs. The U.S. wind sector employs more than 100,000 workers, and wind turbine technician is one of the fastest 
+              growing American jobs. According to the Wind Vision Report, wind has the potential to support more than 600,000 jobs in 
+              manufacturing, installation, maintenance, and supporting services by 2050. Wind enables U.S. industry growth and U.S.
+              competitiveness.</p>
+            <br>
+            <p>New wind projects account for annual investments of over $10 billion in the U.S. economy. The United States has a vast domestic 
+            resources and a highly-skilled workforce, and can compete globally in the clean energy economy. It's a clean fuel source. Wind 
+            energy doesn't pollute the air like power plants that rely on combustion of fossil fuels, such as coal or natural gas, which emit 
+            particulate matter, nitrogen oxides, and sulfur dioxide-causing human health problems and economic damages. Wind turbines don't 
+            produce atmospheric emissions that cause acid rain, smog, or greenhouse gases.</p>
+            <br>
+            <p>Wind is a domestic source of energy. The nation's wind supply is abundant and  inexhaustible. Over the past 10 years, U.S. wind 
+            power capacity has grown 15% per year, and wind is now the largest source of renewable power in the United States. It's sustainable. 
+            Wind is actually a form of solar energy. Winds are caused by the heating of the atmosphere by the sun, the rotation of the Earth, 
+            and the Earth's surface irregularities. For as long as the sun shines and the wind  blows, the energy produced can be harnessed to 
+            send power across the grid. 
+            </p>
+            <br>
+            <p>Wind turbines can be built on existing farms or ranches. This greatly benefits the economy in rural areas, where most of the best 
+            wind sites are found.  Farmers and ranchers can continue to work the land because the wind turbines  use only a fraction of the land. 
+            Wind power plant owners make rent payments  to the farmer or rancher for the use of the land, providing landowners with additional 
+            income.</p>
+            <br>
+            <br>
+            <h4>CHALLENGES OF WIND POWER:</h4>
+            <br>
+            <p>Wind power must still compete with conventional generation sources on a  cost basis.  Even though the cost of wind power has 
+            decreased dramatically  in the past several decades, wind projects must be able to compete economically with the lowest-cost 
+            source of electricity, and some locations may not be windy enough to be cost competitive.</p>
+            <br>
+            <p>Good land-based wind sites are often located in remote locations, far from  cities where the electricity is needed. Transmission 
+            lines must be built to  bring the electricity from the wind farm to the city. However, building just  a few already-proposed transmission
+            lines could significantly reduce the  costs of expanding wind energy.</p>
+            <br>
+            <p>
+            Wind resource development might not be the most profitable use of the land.  Land suitable for wind-turbine installation must
+            compete with alternative uses  for the land, which might be more highly valued than electricity generation. Turbines might cause 
+            noise and aesthetic pollution.</p>
+            <br>
+            <p>Although wind power plants have relatively little impact on the environment  compared to conventional power plants, concern exists
+            over the noise produced by the turbine blades and visual impacts to the landscape.
+            </p>
+            <br>
+            <p>Wind plants can impact local wildlife. Birds have been killed by flying into spinning turbine blades. Most of these problems have 
+            been resolved or greatly reduced through technology development or by properly siting wind plants. Bats have also been killed by 
+            turbine blades, and research is  ongoing to develop and improve solutions to reduce the impact of wind turbines on these species. </p>
+            <br>
+            <p>Like all energy sources, wind projects can alter the habitat on which they are built, which may alter the suitability of that habitat for
+            certain species.</p>
+            <br>
+            <br>
+            <br>
+            <p>Click on the image below to learn about  Wind Power in Australia.</p>
+            <br>
+            <div class='calign'>
+                  <img id='kangaroo-image' src='/images/kangaroo-wind-farm.png' alt='Image of kangaroo standing in front of wind turbines.'>
+            </div>
+                <h4>Wind Power in Australia</h4>
+                <p>(Open in a new window: https://arena.gov.au/renewable-energy/wind/)</p>
+            <p>Click on the image below to learn about the types and applications of Wind Turbines. </p>
+            <br>
+            <div class='calign'>
+                  <img id='wind-turbine-image' src='/images/offshore-wind-turbines.png' alt='Image of wind turbines in the ocean?.'>
+            </div>
+                <h4>Types and Applications of Wind Turbines</h4>
+                <p>(Open in a new window: https://www.energy.gov/eere/wind/how-do-wind-turbines-work/)</p>
+            
 
           </div>
 
